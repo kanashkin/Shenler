3de7d3b4c243b9c7bbe4af611ac59a0f46a5a0e7
Alexander <kanashkin_ad@mail.ru>
2023-07-02 16:03:15 +0300

    ready product card

--- js/app.js
+++ js/app.js
@@ -1,12 +1,47 @@
-const certSwiper = new Swiper('.cert-swiper', {
-    navigation: {
-        nextEl: '.cert-button-next',
-        prevEl: '.cert-button-prev'
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    },
-    slidesPerView: 3,
-    spaceBetween: 30
-});
+const dropDownMenu = () => {
+    const menuBtn = document.querySelector('.sorting-button'),
+        menuBlock = document.querySelector('.sorting-items-block')
+
+    menuBtn.addEventListener('click', () => {
+        menuBlock.classList.toggle('sorting-items-block-active')
+        menuBtn.classList.toggle('sorting-button-active')
+    })
+}
+
+const selectDropItem = () => {
+    const menuItems = document.querySelectorAll('.sorting-items-block .sorting-item'),
+        menuBlock = document.querySelector('.sorting-items-block'),
+        menuItemActiveUp = document.querySelector('.sorting-up-block .sorting-item-active')
+
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', e => {
+
+            if (menuBlock.classList.contains('sorting-items-block-active')) {
+                menuItems.forEach(item => {
+                    item.classList.remove('sorting-item-active')
+                })
+                e.target.classList.add('sorting-item-active')
+                let activeText = e.target.textContent
+                menuItemActiveUp.textContent = activeText
+            }
+            
+        })
+    })
+}
+
+dropDownMenu()
+selectDropItem()
+
+// const certSwiper = new Swiper('.cert-swiper', {
+//     navigation: {
+//         nextEl: '.cert-button-next',
+//         prevEl: '.cert-button-prev'
+//     },
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true
+//     },
+//     slidesPerView: 3,
+//     spaceBetween: 30
+// });
