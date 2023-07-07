986b2c776aab9b5002c47b1fd2052782a196513a
Alexander <kanashkin_ad@mail.ru>
2023-07-07 16:50:37 +0300

    ready footer-mobile at first page

--- js/changeMeta.js
+++ js/changeMeta.js
@@ -2,7 +2,7 @@ const changeMeta = () => {
     window.addEventListener('resize', () => {
         let metaTag = document.querySelector('.meta-viewport')
         if (window.outerWidth <= 450) {
-            metaTag.setAttribute('content', 'width=device-width, initial-scale=1.0')
+            metaTag.setAttribute('content', 'width=320')
         } else if (window.outerWidth > 450) {
             metaTag.setAttribute('content', 'width=1920')
         }
